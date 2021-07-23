bc61a80a4bf41044d249c6fafa9a961c72cff90c
unknown <benclayton1@hotmail.com>
2021-07-23 14:33:47 +0100

    standard acceptance criteria fulfilled

--- cypress/integration/news.spec.js
+++ cypress/integration/news.spec.js
@@ -2,6 +2,7 @@ describe('Home page', function(){
     
     beforeEach(function(){
         cy.intercept("https://content.guardianapis.com/world/2021/mar/22/*", {fixture: 'covid.json'})
+        cy.intercept("https://content.guardianapis.com/football/2021/jun/05/*", {fixture: 'villa.json'})
     })
 
     it('should display a headline', function(){
@@ -22,7 +23,7 @@ describe('Home page', function(){
     it('should display 3 headlines and 3 corresponding images', function(){
         cy.visit('/')
         cy.get('#headline3').should('contain', 'Aston Villa agree £30m deal for Norwich midfielder Emiliano Buendía')
-        cy.get('#headline4').should('contain', 'third headline')
+        cy.get('#headline4').should('contain', 'The best thing Jeremy’s done: why I can’t wait for more Clarkson’s Farm')
         cy.get('#img2').should('have.attr', 'alt')
         cy.get('#img3').should('have.attr', 'alt')
     })
--- src/news.js
+++ src/news.js
@@ -4,12 +4,22 @@ import $ from 'jquery';
 class News extends Component{
     constructor(props){
         super(props)
-        this.state = {api: {response: {content: {fields: {}}}}}
+        this.state = {
+            api1: {response: {content: {fields: {}}}},
+            api2: {response: {content: {fields: {}}}},
+            api3: {response: {content: {fields: {}}}}
+        }
     }
 
     componentDidMount(){
         $.get("https://content.guardianapis.com/world/2021/mar/22/link-between-diabetes-and-coronavirus-infections?show-fields=body&api-key=bf8daced-5acd-41f7-9836-fb4131e480cf&show-fields=thumbnail", (data) => {
-        this.setState({api: data})
+        this.setState({api1: data})
+    })
+        $.get("https://content.guardianapis.com/football/2021/jun/05/aston-villa-agree-30m-deal-for-norwich-midfielder-emiliano-buendia?api-key=bf8daced-5acd-41f7-9836-fb4131e480cf&show-fields=thumbnail", (data) => {
+        this.setState({api2: data})
+    })
+        $.get("https://content.guardianapis.com/tv-and-radio/2021/jul/22/clarksons-farm-best-thing-jeremy-clarkson-has-done-amazon?api-key=bf8daced-5acd-41f7-9836-fb4131e480cf&show-fields=thumbnail", (data) => {
+        this.setState({api3: data})
     })
     }
 
@@ -17,8 +27,13 @@ class News extends Component{
         return(
             <div>
                 <h3 id='headline1' hidden="yes">This is a headline</h3>
-                <h1 id='headline2' title="apiHead" className='fontFamily'>{this.state.api.response.content.webTitle}</h1>
-                <img src={this.state.api.response.content.fields.thumbnail} id='img1' alt='image1'></img>
+                <h1 id='headline2' title="apiHead" className='fontFamily'>{this.state.api1.response.content.webTitle}</h1>
+                <img src={this.state.api1.response.content.fields.thumbnail} id='img1' alt='image1'></img>
+                <h1 id='headline3' title='apiHead2' className='fontFamily'>{this.state.api2.response.content.webTitle}</h1>
+                <img src={this.state.api2.response.content.fields.thumbnail} id='img2' alt='image2'></img>
+                <h1 id='headline4' title='apiHead3' className='fontFamily'>{this.state.api3.response.content.webTitle}</h1>
+                <img src={this.state.api3.response.content.fields.thumbnail} id='img3' alt='image3'></img>
+
             </div>
         )
     }
